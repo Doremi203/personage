fe7cdeba24dda3fc9d8520a20c1bbabe7c6400e6
klysin <klysin@ozon.ru>
2026-01-21 16:22:02 +0300

    [main] use separate db for tests

--- backend/Personage.Auth/Personage.Auth.Tests/Infrastructure/TestApplicationFactory.cs
+++ backend/Personage.Auth/Personage.Auth.Tests/Infrastructure/TestApplicationFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -9,6 +10,7 @@ using Moq;
 using Personage.Auth.Api;
 using Personage.Auth.Bll.Services;
 using Personage.Auth.Domain.Interfaces;
+using Personage.Auth.Migrations.Runner;
 using Personage.Auth.Tests.Infrastructure.Repositories;
 
 namespace Personage.Auth.Tests.Infrastructure;
@@ -16,6 +18,8 @@ namespace Personage.Auth.Tests.Infrastructure;
 public sealed class TestApplicationFactory(Action<IServiceCollection> overrideServices) : WebApplicationFactory<Program>
 {
     public Mock<HttpMessageHandler> HttpMessageHandlerMock { get; } = new();
+    private bool _isMigrationsApplied;
+    private readonly object _lock = new();
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -39,5 +43,34 @@ public sealed class TestApplicationFactory(Action<IServiceCollection> overrideSe
             
             overrideServices.Invoke(services);
         });
+    }
+    
+    protected override void ConfigureClient(HttpClient client)
+    {
+        base.ConfigureClient(client);
+        
+
+        lock (_lock)
+        {
+            if (_isMigrationsApplied) return;
+            
+            RunMigrationsAsync().GetAwaiter().GetResult();
+            _isMigrationsApplied = true;
+        }
+    }
+
+    private async Task RunMigrationsAsync()
+    {
+        using var scope = Services.CreateScope();
+        var migrationRunner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+        
+        try
+        {
+            await migrationRunner.RunMigrations();
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("Failed to run test database migrations", ex);
+        }
     }
 }
